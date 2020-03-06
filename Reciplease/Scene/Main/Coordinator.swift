db244c012c15757cc8b96211d97cfaed93d0d5b3
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2020-03-06 17:47:38 +0100

    coordinator added

---
+++ Reciplease/Scene/Main/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  Reciplease
+//
+//  Created by Fabrice Etiennette on 21/02/2020.
+//  Copyright © 2020 Fabrice Etiennette. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    func start()
+}
